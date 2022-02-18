7a28f2d6e543f966a7d41bfbf1c62492f1528666
juanmanuelynz <juanmanuelynz@gmail.com>
2022-02-18 10:39:12 +0200

    add image gallery

--- scripts/script.js
+++ scripts/script.js
@@ -98,7 +98,110 @@ document.addEventListener("DOMContentLoaded", function (event) {
 /////////////////////////////////////////////////////////////////////
 ////////////////////  - link underline effect  //////////////////////
 /////////////////////////////////////////////////////////////////////
-    
+
+/////////////////////////////////////////////////////////////////////
+////////////////////  + Gallery implementation  /////////////////////
+/////////////////////////////////////////////////////////////////////
+
+const images = [
+    {
+        "id": 0,
+        "title": "Nekkhamma",
+        "image": "/assets/gallery/1 Nekkhamma.jpg",
+        "description": "",
+    },
+    {
+        "id": 1,
+        "title": "Sila",
+        "image": "/assets/gallery/2 Sila.jpg",
+        "description": "",
+    },
+    {
+        "id": 2,
+        "title": "Dana",
+        "image": "/assets/gallery/3 Dana.jpg",
+        "description": "",
+    },
+    {
+        "id": 3,
+        "title": "Pañña",
+        "image": "/assets/gallery/4 Pañña.jpg",
+        "description": "",
+    },
+    {
+        "id": 4,
+        "title": "Viriya",
+        "image": "/assets/gallery/5 Viriya.jpg",
+        "description": "",
+    },
+    {
+        "id": 5,
+        "title": "Kshanti",
+        "image": "/assets/gallery/6 Kshanti.jpg",
+        "description": "",
+    },
+    {
+        "id": 6,
+        "title": "Sacca",
+        "image": "/assets/gallery/7 Sacca.jpg",
+        "description": "",
+    },
+    {
+        "id": 7,
+        "title": "Addhitthana",
+        "image": "/assets/gallery/8 Addhitthana.jpg",
+        "description": "",
+    },
+    {
+        "id": 8,
+        "title": "Metta",
+        "image": "/assets/gallery/9 Metta.jpg",
+        "description": "",
+    },
+    {
+        "id": 9,
+        "title": "Upekka",
+        "image": "/assets/gallery/10 Upekka.jpg",
+        "description": "",
+    },
+];
+
+//MODAL +
+var modal = document.getElementById("gallery-modal");
+
+var modalImg = document.getElementById("modal-img");
+var captionText = document.getElementById("caption");
+
+modal.onclick = function() { 
+  modal.style.display = "none";
+}
+//MODAL -
+
+//Create gallery
+let listHTML = document.getElementById("gallery");
+
+for (let item of images) {
+    let itemHTML = document.createElement("div");
+    itemHTML.classList.add("gallery-item");
+
+    //add image
+    let imageHTML = document.createElement("img");
+    imageHTML.src = item.image;    
+    imageHTML.onclick = function() {
+        modal.style.display = "flex";
+        modalImg.src = this.src;
+        captionText.innerHTML = item.title;
+    };
+
+    itemHTML.appendChild(imageHTML);
+
+    listHTML.appendChild(itemHTML);
+}
+
+/////////////////////////////////////////////////////////////////////
+////////////////////  - Gallery implementation  //////////////////////
+/////////////////////////////////////////////////////////////////////    
+
     
     // window load event makes sure image is 
     // loaded before running animation
