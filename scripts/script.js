cd3bae00074485b955684acd7dd87747886f138f
juanmanuelynz <juanmanuelynz@gmail.com>
2022-03-11 20:16:43 +0200

    implement Vue instead of Vanilla JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,246 +1,122 @@
+// Configure Vue here
+const VueApp = {
+    // Put variables here
+    data() {
+        return {
+            showModal: false,
+            modal: {
+                src: '',
+                caption: ''
+            },
+            gallery: [
+                {
+                    title: "Nekkhamma",
+                    src: "/assets/gallery/1 Nekkhamma.jpg",
+                    description: "",
+                },
+                {
+                    title: "Sila",
+                    src: "/assets/gallery/2 Sila.jpg",
+                    description: "",
+                },
+                {
+                    title: "Dana",
+                    src: "/assets/gallery/3 Dana.jpg",
+                    description: "",
+                },
+                {
+                    title: "Pañña",
+                    src: "/assets/gallery/4 Pañña.jpg",
+                    description: "",
+                },
+                {
+                    title: "Viriya",
+                    src: "/assets/gallery/5 Viriya.jpg",
+                    description: "",
+                },
+                {
+                    title: "Kshanti",
+                    src: "/assets/gallery/6 Kshanti.jpg",
+                    description: "",
+                },
+                {
+                    title: "Sacca",
+                    src: "/assets/gallery/7 Sacca.jpg",
+                    description: "",
+                },
+                {
+                    title: "Addhitthana",
+                    src: "/assets/gallery/8 Addhitthana.jpg",
+                    description: "",
+                },
+                {
+                    title: "Metta",
+                    src: "/assets/gallery/9 Metta.jpg",
+                    description: "",
+                },
+                {
+                    title: "Upekka",
+                    src: "/assets/gallery/10 Upekka.jpg",
+                    description: "",
+                },
+            ],
+            shops: {
+                current_fc: 'assets/shop/fc_0.jpg',
+                current_tl: 'assets/shop/tl_0.jpg',
+                flashcookie: 4,
+                threadless: 5
+            }
+        }
+    },    
+  
+    // Functions go here
+    methods: {
+        openModal(i) {
+            this.modal.src = this.gallery[i].src;
+            this.modal.caption = this.gallery[i].title;
 
-// wait until DOM is ready
-document.addEventListener("DOMContentLoaded", function (event) {
+            this.showModal = true;
+        },
+        closeModal() {
+            this.showModal = false;
 
-/////////////////////////////////////////////////////////////////////
-///////////////////  + Grain background effect  /////////////////////
-/////////////////////////////////////////////////////////////////////
-    let grainAnim = anime({
-                        targets: '#grain',
-                        keyframes: [
-                            {backgroundPosition: '-1px bottom'},
-                            {backgroundPosition: 'left .5px'}, 
-                            {backgroundPosition: '0px top'}, 
-                            {backgroundPosition: 'right -1px'}
-                            ],
-                        duration: 600,
-                        easing: 'steps(4)',
-                        loop: true,
-                        autoplay: true
-                    });
-    
-/////////////////////////////////////////////////////////////////////
-///////////////////  - Grain background effect  /////////////////////
-/////////////////////////////////////////////////////////////////////
+            this.modal.src = '';
+            this.modal.caption = '';
+        },
+        updateShops(){
+            let tlIndex = 0;
+            let fcIndex = 0;
+            let merch = this.shops;
+            setInterval(function () {             
+                fcIndex < merch.flashcookie-1 ? fcIndex++ : fcIndex = 0;
+                merch.current_fc = 'assets/shop/fc_'+ fcIndex +'.jpg';
+            
+                tlIndex < merch.threadless-1 ? tlIndex++ : tlIndex = 0;
+                merch.current_tl = 'assets/shop/tl_'+ tlIndex +'.jpg';
+             }, 3000);
+        }
+    },
 
-/////////////////////////////////////////////////////////////////////
-///////////////////  + particular button effect  ////////////////////
-/////////////////////////////////////////////////////////////////////
-
-    let seed = document.querySelector(".seed");
-    
-    /*Seed ANIMATION*/
-    seed.addEventListener("mouseenter", function () {
-        anime({
-            targets: this,
-            scale: 1.1,
-            fill: '#FBB189'
+    // Code that will run as soon as app is ready
+    mounted() {
+        //Anim background
+        let grainAnim = anime({
+            targets: '#grain',
+            keyframes: [
+                {backgroundPosition: '-1px bottom'},
+                {backgroundPosition: 'left .5px'}, 
+                {backgroundPosition: '0px top'}, 
+                {backgroundPosition: 'right -1px'}
+                ],
+            duration: 600,
+            easing: 'steps(4)',
+            loop: true,
+            autoplay: true
         });
-    });
-    
-    seed.addEventListener("mouseleave", function () {
-        anime({
-            targets: this,
-            scale: 1,
-            fill: '#10100F'
-        });
-    });
-    
-    seed.addEventListener("click", function () {
         
-    });
-    
-/////////////////////////////////////////////////////////////////////
-///////////////////  - particular button effect  ////////////////////
-/////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////////////
-////////////////////  + link underline effect  //////////////////////
-/////////////////////////////////////////////////////////////////////    
-    
-    function underlineAnim(link, e, index) {
-        let underline = link.querySelector(".underline");
-
-        anime({
-            targets: underline,
-            width: "100%",
-            left: "0%",
-            easing: 'easeOutQuint'
-        });
+        this.updateShops();
     }
-
-    function leaveUnderlineAnim(link, e) {
-        let underline = link.querySelector(".underline");
-
-        anime({
-            targets: underline,
-            width: "0%",
-            left: "0%",
-            easing: 'easeInQuint'
-        });
-    }
-
-    // Get all links
-    let workLinks = document.querySelectorAll(".link");
-    
-    workLinks.forEach(activateLinks);
-        
-    function activateLinks(link, index, value) {
-        
-        link.addEventListener("mouseenter", function(e) {
-            underlineAnim(link, e);
-        });
-
-        link.addEventListener("mouseleave", function(e) {
-            leaveUnderlineAnim(link, e);
-        });
-    }
-/////////////////////////////////////////////////////////////////////
-////////////////////  - link underline effect  //////////////////////
-/////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////////////
-////////////////////  + Gallery implementation  /////////////////////
-/////////////////////////////////////////////////////////////////////
-
-const images = [
-    {
-        "id": 0,
-        "title": "Nekkhamma",
-        "image": "/assets/gallery/1 Nekkhamma.jpg",
-        "description": "",
-    },
-    {
-        "id": 1,
-        "title": "Sila",
-        "image": "/assets/gallery/2 Sila.jpg",
-        "description": "",
-    },
-    {
-        "id": 2,
-        "title": "Dana",
-        "image": "/assets/gallery/3 Dana.jpg",
-        "description": "",
-    },
-    {
-        "id": 3,
-        "title": "Pañña",
-        "image": "/assets/gallery/4 Pañña.jpg",
-        "description": "",
-    },
-    {
-        "id": 4,
-        "title": "Viriya",
-        "image": "/assets/gallery/5 Viriya.jpg",
-        "description": "",
-    },
-    {
-        "id": 5,
-        "title": "Kshanti",
-        "image": "/assets/gallery/6 Kshanti.jpg",
-        "description": "",
-    },
-    {
-        "id": 6,
-        "title": "Sacca",
-        "image": "/assets/gallery/7 Sacca.jpg",
-        "description": "",
-    },
-    {
-        "id": 7,
-        "title": "Addhitthana",
-        "image": "/assets/gallery/8 Addhitthana.jpg",
-        "description": "",
-    },
-    {
-        "id": 8,
-        "title": "Metta",
-        "image": "/assets/gallery/9 Metta.jpg",
-        "description": "",
-    },
-    {
-        "id": 9,
-        "title": "Upekka",
-        "image": "/assets/gallery/10 Upekka.jpg",
-        "description": "",
-    },
-];
-
-//MODAL +
-var modal = document.getElementById("gallery-modal");
-
-var modalImg = document.getElementById("modal-img");
-var captionText = document.getElementById("caption");
-
-modal.onclick = function() { 
-  modal.style.display = "none";
-}
-//MODAL -
-
-//Create gallery
-let listHTML = document.getElementById("gallery");
-
-for (let item of images) {
-    let itemHTML = document.createElement("div");
-    itemHTML.classList.add("gallery-item");
-
-    //add image
-    let imageHTML = document.createElement("img");
-    imageHTML.src = item.image;    
-    imageHTML.onclick = function() {
-        modal.style.display = "flex";
-        modalImg.src = this.src;
-        captionText.innerHTML = item.title;
-    };
-
-    itemHTML.appendChild(imageHTML);
-
-    listHTML.appendChild(itemHTML);
-}
-
-/////////////////////////////////////////////////////////////////////
-////////////////////  - Gallery implementation  /////////////////////
-/////////////////////////////////////////////////////////////////////    
-
-/////////////////////////////////////////////////////////////////////
-/////////////////////////  + Shop Gallery  //////////////////////////
-/////////////////////////////////////////////////////////////////////    
-
-//Create Flashcookie slides
-let fcGallery = document.getElementById("flashcookie");
-const fcImages = fcGallery.getElementsByTagName('img');
-let fcIndex = 0;
-
-//Create Threadless slides
-let tlGallery = document.getElementById("threadless");
-const tlImages = tlGallery.getElementsByTagName('img');
-let tlIndex = 0;
-
-function updateShops() {
-    fcImages[fcIndex].classList.add('hidden');
-    fcIndex < fcImages.length-1 ? fcIndex++ : fcIndex = 0;
-    fcImages[fcIndex].classList.remove('hidden');
-
-    tlImages[tlIndex].classList.add('hidden');
-    tlIndex < tlImages.length-1 ? tlIndex++ : tlIndex = 0;
-    tlImages[tlIndex].classList.remove('hidden');
-}
-
-setInterval(updateShops, 3000);
-
-
-/////////////////////////////////////////////////////////////////////
-/////////////////////////  - Shop Gallery  //////////////////////////
-/////////////////////////////////////////////////////////////////////    
-
-    
-    // window load event makes sure image is 
-    // loaded before running animation
-    window.onload = function() {        
-//         $('.navbar-nav>li>a').on('click', function(){
-//            $('.navbar-collapse').collapse('hide');
-//        });
-    };
-    
-    
-});
+  };
+  
+  // Initialize Vue
+  Vue.createApp(VueApp).mount('#app');
