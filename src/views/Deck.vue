b1b10dca477b611ba738b5374309950b6dd9af4b
juanmanuelynz <juanmanuelynz@gmail.com>
2022-08-03 13:27:16 -0300

    implement deck's back button

--- src/views/Deck.vue
+++ src/views/Deck.vue
@@ -1,10 +1,17 @@
 <script setup>
     import DeckTemplate from '../components/DeckTemplate.vue';
     import DeckTemplateSkeleton from '../components/DeckTemplateSkeleton.vue';
+
+    import { useRouter } from 'vue-router';
+    const router = useRouter();
+
+    const navigate = (url) => {
+        router.push({ path: url, replace: true });
+    };
 </script>
 
 <template>
-    <div class="content deck">
+    <div class="content deck">        
         <main id="cardDeck">        
             <Suspense>
                 <template #default>
@@ -15,12 +22,22 @@
                 </template>
             </Suspense>
         </main>
+        <button @click="navigate('/')"></button>
     </div>
 </template>
 
 <style>
     .content.deck {
         padding-top: 9vh;
+    }
+
+    .deck button {   
+        margin-top: 48px;     
+        height: 40px;
+        background-repeat: no-repeat;
+        background-color: transparent;
+        background-image: url('../assets/icon/Back.svg');
+        filter: drop-shadow(0px 2px 6px rgba(16, 16, 15, 0.36));
     }
 
     /* DESKTOP */
