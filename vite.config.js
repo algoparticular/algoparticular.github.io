e023bb34d5836b013e1f8e134d352225d5c794b2
juanmanuelynz <juanmanuelynz@gmail.com>
2022-09-09 17:25:05 -0300

    remove tailwind

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/algoparticular.github.io/praxis/',
   plugins: [
     vue(),
     // vueI18n({
